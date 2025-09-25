8a6df4f1a706985fbb30f26ecebb89b462b64557
Arun KS <caspers3d@gmail.com>
2025-09-26 03:18:47 +0530

    files updated

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/portfolio"
+  build: {
+    outDir: 'build', // default is 'dist'
+  },
+  base: "https://arunks636.github.io/portfolio/"
 })
